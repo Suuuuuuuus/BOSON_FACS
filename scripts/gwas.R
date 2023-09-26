890dbbfabef5d0aae8007ddd7370eff9802f9c08
Suuuuuuuus <bjzzp01@163.com>
2023-09-26 08:55:47 +0100

    Restructure

--- scripts/gwas.R
+++ scripts/gwas.R
@@ -1,6 +1,8 @@
 args <- commandArgs(trailingOnly = TRUE)
 marker <- args[1]
 plot <- args[2]
+indir <- args[3]
+plotdir <- args[4]
 
 library(qqman)
 library(data.table)
@@ -9,10 +11,9 @@ library(CMplot)
 source("../scripts/kactk.R")
 
 #setwd("Desktop/Boson FACS/boson_vcf/")
-
 #marker = "CD3+CD57+"
 
-res_file = paste0("results/", marker, ".assoc.linear")
+res_file = paste0(indir , marker, ".assoc.linear")
 
 df = fread(res_file)
 res = data.frame(df$SNP, df$CHR, df$BP, df$P)
@@ -20,7 +21,7 @@ names(res)<-c("SNP", "CHR", "BP", "P")
 
 if (plot == "TRUE"){
 
-  setwd("../graphs/facs-host/baseline/")
+  setwd(plotdir)
 
   CMplot(res,plot.type = "q",threshold = 0.05, file = "jpg", file.name = marker, dpi = 300)
   CMplot(res,plot.type = "m", LOG10=TRUE, amplify = T, col = c("grey30", "grey60"), 
@@ -30,6 +31,6 @@ if (plot == "TRUE"){
 
 est = estlambda(df$P)$estimate
 res = paste0(marker, "\t", est)
-write(res, file = "../../boson_vcf/genomic_inflation_factor.txt", append = TRUE)
+write(res, file = "../../boson_vcf/genomic_inflation_factor_all.txt", append = TRUE)
 
 #setwd(dirname(normalizePath(getwd())))
--- scripts/parse_plink.py
+++ scripts/parse_plink.py
@@ -1,7 +1,8 @@
+import sys
 import FACSus as fs
 import pandas as pd
 
-markers = pd.read_csv('markers.txt', header = None).rename(columns = {0: 'marker'})
-markers = markers.marker.to_list()
-for i in markers:
-    fs.parse_plink('results/' + i + '.assoc.linear', True, 'results/' + i + '.baseline.gwas.txt')
+marker = str(sys.argv[1])
+indir = str(sys.argv[2])
+
+fs.parse_plink(indir + marker + '.assoc.linear', True, indir + marker + '.baseline.gwas.txt')
